2dcc72185291b532574903f8eb3f1e776512dbfa
Fakultet Organizacionih Nauka <lazarpopadic09@gmail.com>
2019-12-10 13:28:37 +0100

    lacooniiii

--- todo-frontend/src/store/index.js
+++ todo-frontend/src/store/index.js
@@ -4,7 +4,10 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+
+    baseurl:'http://localhost:8080/todotest/todo-backend'
+  },
   mutations: {},
   actions: {},
   modules: {}
